a3c3141c972bc82eaa0e945498533f8116e30736
pablorc-lab <prambladoc@gmail.com>
2024-11-28 18:33:26 +0100

    cambiado home

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
-import Populars from "./popular_section/populars";
+import Anf_card from "../users_cards/anf_card";
+import Inq_card from "../users_cards/inq_card";
 
 import "./home.css"
 
@@ -39,128 +40,202 @@ export default function Home_page() {
   }, []); 
 
 	return (
-		<>
+    <>
       {/*Cabecera*/}
-			<header>
-				<figure className="header_logo">
-					<img src="images/logos/logo_verde.png" alt="Logo Bearfrens" width="150" />
-					<figcaption>Bearfrens</figcaption>
-				</figure>
-				<div className="header_menu">
-					<div id="menu_icon" >
-						<img src="images/logos/menu.png" alt="Icono de menú" />
-					</div>
-
-					<nav id="header_nav">
-						<a>Inicio</a>
-						<a>Alojamientos</a>
-						<a>Inquilinos</a>
-						<a>Soporte</a>
-					</nav>
-
-					<div className="header_user">
-						<img src="images/logos/logo_usuario_blanco.png" alt="Logo usuario" width="50" />
-						<a href="#" className="client_access">Acceder</a>
-					</div>
-				</div>
-			</header>
-
-			<main>
-
-        {/*?LOGO INICIAL*/}
-        <div className="main_container">
-          <div id="logo_main">
-            <img src="images/logos/logo_verde.png" alt="Logo Bearfrens" width={150} />
+      <header>
+        <img 
+          className="header_logo"
+          src="images/logos/logo_verde.png"
+          alt="Logo Bearfrens"
+          width="150"
+        />
+        <div className="header_menu">
+          <div id="menu_icon">
+            <img src="images/logos/menu.png" alt="Icono de menú" />
           </div>
 
+          <nav id="header_nav">
+            <a>Inicio</a>
+            <a>Alojamientos</a>
+            <a>Inquilinos</a>
+            <a>Soporte</a>
+          </nav>
 
-          {/*?BOTON INQUILINOS Y VIAJEROS*/}
-          <section className="button_section">
-            <div className="button_container">
-              <h2>Encuentra el alojamiento perfecto</h2>
-              <div id="btn_alojamientos">
-                <img src="images/logos/anfitrion.png" alt="Icono casa" />
-                <p>Hospedajes</p>
-              </div>
+          <div className="header_user">
+            <img
+              src="images/logos/logo_usuario_blanco.png"
+              alt="Logo usuario"
+              width="50"
+            />
+            <a href="#" className="client_access">
+              Acceder
+            </a>
+          </div>
+        </div>
+      </header>
+
+      <main>
+        {/*?LOGO INICIAL*/}
+        <section className="main_container">
+          <figure id="logo_main">
+            <img src="images/logos/logo_verde.png" alt="Logo Bearfrens" width="150"/>
+            <figcaption>Bearfrens</figcaption>
+          </figure>
+        	<p className="main_descrpt">Donde viajeros y anfitriones se encuentran</p>
+        </section>
+
+        {/*?SECCION DE LOS ANFITRIONES*/}
+        <section className="card_section anf_card">
+          <div className="button_container">
+            <h2>Encuentra el alojamiento perfecto</h2>
+            <div id="btn_alojamientos">
+              <img src="images/logos/anfitrion.png" alt="Icono casa" />
+              <p>Hospedajes</p>
             </div>
-            <div className="button_container">
-              <h2>Conecta con todo tipo de personas</h2>
-              <div id="btn_inquilinos">
+          </div>
+          
+          <div className="card_container">
+            <Anf_card
+              Casa_img="/images/landing_page/casa_1.webp"
+              Perfil_img="/images/landing_page/persona_1.webp"
+              Nombre="Pablo"
+              Gustos_imgs={[
+                "/images/usuarios/Gustos/baseball.png",
+                "/images/usuarios/Gustos/pesca.png",
+                "/images/usuarios/Gustos/poker.png",
+              ]}
+              Valoracion={4.91}
+              Ubicacion="Barcelona"
+              Precio="300"
+              Descripcion="Amante de la aventura y los viajes improvisados"
+              prof_number="prof_1"
+            />
+          </div>
+        </section>
+        
+        {/*?SECCION DE LOS VIAJEROS*/}
+        <section className="card_section inq_card">
+          <div className="card_container">
+            <Inq_card
+              Perfil_img="images/landing_page/persona_3.webp"
+              Nombre="Juanma"
+              Valoracion="4.51"
+              Num_viajes="82"
+              Edad="56"
+              Profesion="Mécanico"
+              Descripcion="Soy un hombre apasionado por conocer nuevas personas y sitios desconocidos"
+              Gustos_imgs={[
+                "/images/usuarios/Gustos/pesca.png",
+                "/images/usuarios/Gustos/social.png",
+                "/images/usuarios/Gustos/baseball.png",
+              ]}
+              prof_number="prof_3"
+            />
+          </div>
+
+          <div className="button_container">
+            <h2>Conecta con personas mundialmente</h2>
+            <div id="btn_inquilinos">
                 <img src="images/logos/inquilino.png" alt="Icono inquilino" />
                 <p>Inquilinos</p>
-              </div>
             </div>
-          </section>	
-        </div>
+          </div>
+        </section>
 
-
-        {/*?SECCION DE LOS POPULARES*/}
-        <Populars/>
-
-
-				{/*?VENTAJAS*/}
+        {/*?VENTAJAS*/}
         <section className="advantages">
-					<h2>Ventajas</h2>
-					<div className="advantages_container">
-						{advantages.map((advantage, index) => (
-							<article className="advantage_item" key={index}>
-								<img src={`/images/landing_page/advantage_${index + 1}.png`} alt={`Advantage ${index + 1}`} className="icon" width="250"/>
-								<div className="advantage_text">
-									<h3>{advantage.title}</h3>
-									<p>{advantage.description}</p>
-								</div>
-							</article>
-						))}
-					</div>
-					<div className="gradient_block"></div>
-				</section>
+          <h2>VENTAJAS</h2>
+          <div className="advantages_container">
+            {advantages.map((advantage, index) => (
+              <article className="advantage_item" key={index}>
+                <img
+                  src={`/images/landing_page/advantage_${index + 1}.png`}
+                  alt={`Advantage ${index + 1}`}
+                  className="icon"
+                  width="250"
+                />
+                <div className="advantage_text">
+                  <h3>{advantage.title}</h3>
+                  <p>{advantage.description}</p>
+                </div>
+              </article>
+            ))}
+          </div>
+        </section>
 
-
-				{/*?OPINIONES*/}
-				<section className="opinions">
-					<h2>Opiniones de nuestros clientes</h2>
-					<div className="opinions_container">
-						{opinions.map(({ text, img, name, role }, index) => (
-							<div key={index} className={`bubble_text ${(index === 1) ? "central" : ""}`}>
-								<p>{text}</p>
-								<div className="prof_opinion">
-									<img className="profile_img" src={img} alt={`Imagen de ${role}`} width="250"/>
-									<div className="info_opinion">
-										<h3>{name}</h3>
-										<p>{role}</p>
-									</div>
-								</div>
-							</div>
-						))}
-					</div>
-				</section>
-			</main>
+        {/*?OPINIONES*/}
+        <section className="opinions">
+          <h2>OPINIONES DE NUESTROS CLIENTES</h2>
+          <div className="opinions_container">
+            {opinions.map(({ text, img, name, role }, index) => (
+              <div
+                key={index}
+                className={`bubble_text ${index === 1 ? "central" : ""}`}
+              >
+                <p>{text}</p>
+                <div className="prof_opinion">
+                  <img
+                    className="profile_img"
+                    src={img}
+                    alt={`Imagen de ${role}`}
+                    width="250"
+                  />
+                  <div className="info_opinion">
+                    <h3>{name}</h3>
+                    <p>{role}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </section>
+      </main>
 
       {/*Pie de pagina*/}
-			<footer>
-				{/*Logo*/}
-				<figure id="logo_footer">
-					<img src="images/logos/logo_verde.png" alt="Logo Bearfrens" width={150} />
-					<figcaption>Bearfrens</figcaption>
-				</figure>
-				{/*Información de contacto*/}
-				<div className="contact-info">
-					<div id="text-info">
-						<p>Email: <strong>pabloramblado@correo.ugr.es</strong></p>
-						<p>Teléfono: <strong>+34 666-666-666</strong></p>
-					</div>
-					<div className="social-media">
-						<img src="./images/logos/instagram_logo.png" alt="instagram_logo" width={50} />
-						<img src="./images/logos/x_logo.png" alt="x_logo" width={50} />
-						<img src="./images/logos/youtube_logo.png" alt="youtube_logo" width={50} />
-						<img src="./images/logos/facebook_logo.png" alt="facebook_logo" width={50} />
-					</div>
-				</div>
-				{/*Políticias de privacidad*/}
-				<div className="privacy-policy">
-					<a href="/politicas-de-privacidad">Políticas de Privacidad</a>
-				</div>
-			</footer>
-		</>
-	);
+      <footer>
+        {/*Logo*/}
+        <figure id="logo_footer">
+          <img
+            src="images/logos/logo_blanco.png"
+            alt="Logo Bearfrens"
+            width={150}
+          />
+          <figcaption>Bearfrens</figcaption>
+        </figure>
+        {/*Información de contacto*/}
+        <div className="contact-info">
+          <div id="text-info">
+            <p>
+              Email: <strong>pabloramblado@correo.ugr.es</strong>
+            </p>
+            <p>
+              Teléfono: <strong>+34 666-666-666</strong>
+            </p>
+          </div>
+          <div className="social-media">
+            <img
+              src="./images/logos/instagram_logo.png"
+              alt="instagram_logo"
+              width={50}
+            />
+            <img src="./images/logos/x_logo.png" alt="x_logo" width={50} />
+            <img
+              src="./images/logos/youtube_logo.png"
+              alt="youtube_logo"
+              width={50}
+            />
+            <img
+              src="./images/logos/facebook_logo.png"
+              alt="facebook_logo"
+              width={50}
+            />
+          </div>
+        </div>
+        {/*Políticias de privacidad*/}
+        <div className="privacy-policy">
+          <a href="/politicas-de-privacidad">Políticas de Privacidad</a>
+        </div>
+      </footer>
+    </>
+  );
 }
