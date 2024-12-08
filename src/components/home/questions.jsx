9865ad3d543646a9de20d0ea1db45dff2d05804c
pablorc-lab <prambladoc@gmail.com>
2024-12-08 20:47:46 +0100

    faq

--- src/components/home/questions.jsx
+++ src/components/home/questions.jsx
@@ -4,19 +4,17 @@ export default function Question({ pregunta, respuesta }) {
   const [isOpen, setOpen] = useState(false); 
 
   return (
-    <article className="question_item" onClick={() => setOpen(!isOpen)}>
-      <div className="question_title">
-        <h3>{pregunta}</h3>
+    <article className="question_item">
+      <div className={`question_title ${isOpen ? 'open' : ''}`} onClick={() => setOpen(!isOpen)}>
+      <h3>{pregunta}</h3>
         <img
           src={isOpen ? "/images/landing_page/faq_minus.png" : "/images/landing_page/faq_plus.png"}
           alt={isOpen ? "Icono más" : "Icono menos"}
         />      
       </div>
-      {isOpen && ( 
-        <div className="question_content">
-          <p>{respuesta}</p>
-        </div>
-      )}
+      <div className={`question_content ${isOpen ? 'open' : ''}`}>
+        <p>{respuesta}</p>
+      </div>
     </article>
   );
 }
